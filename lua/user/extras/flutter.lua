e443fb7715907990e9c665130aba794797017e20
Zachary Streeter <zacharylouis42@gmail.com>
2025-04-19 15:35:04 -0500

    added flutter tools

---
+++ lua/user/extras/flutter.lua
@@ -0,0 +1,12 @@
+local M = {
+  "nvim-flutter/flutter-tools.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  config = true,
+}
+
+function M.config() end
+
+return M
