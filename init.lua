f0bef491752879c4dddfbbd6d2c98dff7982f5c7
Zachary Streeter <zacharylouis42@gmail.com>
2025-07-25 12:41:20 -0500

    needed this

---
+++ init.lua
@@ -0,0 +1,9 @@
+require("config.options")
+require("config.keymaps")
+require("config.autocommands")
+require("config.lazy")
+require("config.colorscheme")
+require("config.lsp")
+
+-- Custom functions and maybe future plugins
+require("functions.search-projects")
